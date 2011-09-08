51cf969202464d43866cc341db5e9ee4e9d140af
Hugues Lismonde <hugues@epic.net>
2011-09-08 22:53:08 +0200

    First release

---
+++ ddnsupdate.gemspec
@@ -0,0 +1,17 @@
+Gem::Specification.new do |s|
+  s.name = %q{ddnsupdate}
+  s.version = "0.1.0"
+
+  s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
+  s.authors = [%q{Hugues Lismonde}]
+  s.date = %q{2011-09-08}
+  s.email = %q{hugues@epic.net}
+  s.executables = [%q{ddnsupdate}]
+  s.files = [%q{HISTORY}, %q{LICENSE}, %q{README.md}, %q{bin/ddnsupdate}, %q{lib/ddnsupdate.rb}]
+  s.homepage = %q{https://github.com/epicagency/ddnsupdate/}
+  s.require_paths = [%q{lib}]
+  s.required_ruby_version = Gem::Requirement.new(">= 1.8.6")
+  s.rubygems_version = %q{1.8.5}
+  s.summary = %q{Performs dynamic dns updates}
+end
+
---
+++ lib/ddnsupdate.rb
@@ -0,0 +1,76 @@
+module DDNSUpdate
+  require 'open3'
+  require 'digest/md5'
+  require 'base64'
+  require 'socket'
+  require 'net/http'
+  require 'uri'
+
+  class UpdateError < StandardError; end
+
+  def self.keygen(input)
+    Base64.encode64(Digest::MD5.hexdigest(input.downcase))
+  end
+
+  def self.determine_soa(zone)
+    soa = %x{dig -t SOA #{zone.gsub(/\.$/, "")} +noquestion +nostats +nocmd +noqr +nocomments +noadditional +nottlid}
+    #Split lines into an array, filtering out comments and blanks
+    soa = soa.split("\n").delete_if { |el| el.start_with?(";") || el.empty? }
+    #Split remaining line into whitespace delimited fields
+    soa = soa[0].split(/\s/)
+    #Find the field we actually want, stripping the trailing dot
+    soa[soa.index("SOA") + 1].gsub(/\.$/, "")
+  end
+
+  def self.determine_current_ip(zone, soa=nil)
+    soa = determine_soa(zone) unless !soa.nil?
+    %x{dig @#{soa} #{zone} A +short }
+  end
+
+  def self.determine_local_ip
+    orig, Socket.do_not_reverse_lookup = Socket.do_not_reverse_lookup, true  # turn off reverse DNS resolution temporarily
+
+    UDPSocket.open do |s|
+      s.connect '64.233.187.99', 1
+      s.addr.last
+    end
+  ensure
+      Socket.do_not_reverse_lookup = orig
+  end
+
+  def self.determine_remote_ip
+    Net::HTTP.get(URI.parse('http://www.whatismyip.org/'))
+  end
+
+  def self.update(zone, ip, key, wild = false)
+    soa         = determine_soa(zone)
+    curip       = determine_current_ip(zone, soa)
+
+    if curip != ip
+      delete_seq  = <<-";"
+                    server #{soa}
+                    key #{zone}. #{key}
+                    prereq yxdomain #{zone}.
+                    update delete #{zone}. A
+                    ;
+      delete_seq += "                    update delete *.#{zone}. A\n" if wild
+      delete_seq += "                    send\n"
+
+      create_seq  = <<-";"
+                    server #{soa}
+                    key #{zone}. #{key}
+                    prereq nxdomain #{zone}.
+                    update add #{zone}. 300 A #{ip}
+                    ;
+      create_seq += "                    update add *.#{zone}. 300 A #{ip}\n" if wild
+      create_seq += "                    send\n"
+
+      Open3.popen3("nsupdate") do |stdin, stdout, stderr|
+        stdin << delete_seq << create_seq
+        stdin.close_write
+        err = stderr.read
+        raise UpdateError, err unless err.empty?
+      end
+    end
+  end
+end
